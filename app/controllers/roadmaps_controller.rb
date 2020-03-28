ee382af55b41c6f0face8a0db2ec1f50e7d5bbae
Pierre-Antoine Bauduin <bauduinpa@gmail.com>
2020-03-28 13:02:06 +0100

    changed controller roadmap to redirect after crud

--- app/controllers/roadmaps_controller.rb
+++ app/controllers/roadmaps_controller.rb
@@ -1,9 +1,8 @@
 class RoadmapsController < ApplicationController
   before_action :set_roadmap, only: [:edit, :show, :update, :destroy]
-  before_action :set_roadmaps, only: [:show, :new, :create, :edit, :update]
+  before_action :set_roadmaps, only: [:show, :new, :create, :edit, :update, :destroy]
 
   def show
-    @roadmap = Roadmap.find(params[:id])
   end
 
   def new
@@ -16,8 +15,10 @@ class RoadmapsController < ApplicationController
     @show =
     if @roadmap.save
       redirect_to(@roadmap)
+      flash[:notice] = "Roadmap crée"
     else
       render :new
+      flash[:notice] = "Impossible de créer la roadmap"
     end
   end
 
@@ -26,20 +27,22 @@ class RoadmapsController < ApplicationController
 
   def update
     if @roadmap.update(roadmap_params)
-      render :show
+      redirect_to(@roadmap)
+      flash[:notice] = "Roadmap modifiée"
     else
       render :edit
+      flash[:notice] = "Impossible d'éditer la roadmap"
     end
   end
 
   def destroy
     @roadmap.destroy
-
-    @roadmap = Roadmap.find_by(user: current_user)
-    if @roadmap
-      redirect_to roadmap_path(@roadmap)
+    if !@roadmaps.any?
+      redirect_to(new_roadmap_path)
+      flash[:notice] = "Roadmap supprimée"
     else
-      new_roadmap_path
+      redirect_to(roadmap_path(@roadmaps.last.id))
+      flash[:notice] = "Roadmap supprimée"
     end
   end
 
