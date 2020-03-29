1fa893cb3de27ea6acf1dcd036d954027554b7b0
Pierre-Antoine Bauduin <bauduinpa@gmail.com>
2020-03-29 14:21:44 +0200

    css fixes everywhere

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,7 @@ require("channels")
 import "bootstrap";
 
 import {} from '../scripts/fields'
-import {} from '../scripts/dropdownmenu'
+import {} from '../scripts/dropdownuser'
 import {} from '../scripts/roadmap.js.erb'
-import {} from '../scripts/dropdownshare.js'
+import {} from '../scripts/dropdownshare'
+import {} from '../scripts/dropdownroadmap'
