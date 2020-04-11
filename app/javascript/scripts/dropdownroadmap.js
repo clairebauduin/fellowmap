4c73140f6a0a670169996e0f23be4c2b54034d66
Pierre-Antoine Bauduin <bauduinpa@gmail.com>
2020-04-11 18:22:55 +0200

    better roadmap selector

--- app/javascript/scripts/dropdownroadmap.js
+++ app/javascript/scripts/dropdownroadmap.js
@@ -1,16 +1,11 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 const dropdownRoadmap = document.querySelectorAll(".dropdown-roadmap")[0]
-const roadmapName = document.querySelectorAll(".roadmap-name-ellipsis")[0]
+const roadmapName = document.querySelectorAll(".roadmap-name")[0]
 
 if(typeof(roadmapName) != "undefined") {
   roadmapName.addEventListener("click", function(event){
     dropdownRoadmap.classList.toggle("dropdown-show");
     event.preventDefault();
-  })
-    window.addEventListener("click", function(event){
-    if (!event.target.matches(".roadmap-name-ellipsis")) {
-      dropdownRoadmap.classList.remove("dropdown-show");
-    }
   })
 }
