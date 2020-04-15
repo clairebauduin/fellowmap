5b67a18083f38fb2ceeb66bfbc22dbe469edcca4
Pierre-Antoine Bauduin <bauduinpa@gmail.com>
2020-04-15 21:07:23 +0200

    added vision

---
+++ app/javascript/scripts/vision.js
@@ -0,0 +1,19 @@
+$(document).on('ready turbolinks:load', function() {
+  // initial margin-top for columns
+  $(".admin").css('margin-top',$(".vision-container").height() + 35);
+  $(".preview-guest").css('margin-top',$(".vision-container").height());
+  // dynamic show/hide vision on scroll
+  $(window).scroll(function (event) {
+    if ($(window).scrollTop() > 20) {
+      $(".vision-container").fadeOut(50);
+    }
+    else {
+      $(".vision-container").fadeIn(50);
+    }
+  });
+  //change margin-top if vision gets more height
+  $(".roadmap-vision").keyup(function(ev1) {
+    $(".admin").css('margin-top',$(".vision-container").height() + 35);
+    $(".preview-guest").css('margin-top',$(".vision-container").height());
+  })
+})
