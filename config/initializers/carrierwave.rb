bf697402fbdab37babc0674d76231fd0a2e99cbf
Pierre-Antoine Bauduin <bauduinpa@gmail.com>
2020-04-16 13:30:48 +0200

    env again

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,7 +3,7 @@ CarrierWave.configure do |config|
     provider: 'AWS',
     aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region: 'eu-west-3',
+    region: 'eu-west-3'
   }
   config.fog_directory  = 'fellowmap'
 
